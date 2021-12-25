04f8357971cf2dabd2cd623b118bbb2c50686167
Wayne Rose <e.guishardrose@hotmail.co.uk>
2021-12-25 23:00:30 +0000

    version to test get_information method

--- Metacritic_Webscraper_Latest.py
+++ Metacritic_Webscraper_Latest.py
@@ -1,15 +1,23 @@
 import time 
+import selenium
 from selenium import webdriver 
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 
+'''
+Version of the scraper solely to test that the 
+get_information_from_page() method is returning the correct values as intended. 
+Keep for debugging purposes? 
+'''
 class MetaCriticScraper: 
 
     def __init__(self, driver = webdriver.Chrome()):
         self.driver = driver 
-        self.root = "https://www.metacritic.com/"
+        # Temporary change in root to debug collecting information from the page. Original root =  "https://www.metacritic.com/"
+        self.root = "https://www.metacritic.com/game/wii-u/super-smash-bros-for-wii-u"
         driver.get(self.root)
         self.accept_cookies()
+        self.page_counter = 0
 
         self.xpaths_dict = {'Title': '//*[@class="hover_none"]', 
                    'Platform': '//*[@id="main"]/div/div[1]/div[1]/div[1]/div[2]/span', 
@@ -18,12 +26,12 @@ class MetaCriticScraper:
                    'User_Score': '//*[@id="main"]/div/div[1]/div[1]/div[3]/div/div[2]/div[1]/div[2]/div[1]/div/a/div',
                    'Description': '//li[@class="summary_detail product_summary"]' } 
 
-        self.information_dict =  {'Title' : "", 
-                                  'Platform' : "",  
-                                  'Release Date' : "",      
-                                  'MetaCritic Score' : "",      
-                                  'User_Score': "" ,    
-                                  'Description' : ""}
+        self.information_dict =  {'Title' : None, 
+                                  'Platform' : None,  
+                                  'Release Date' : None,      
+                                  'MetaCritic Score' : None,      
+                                  'User_Score': None ,    
+                                  'Description' : None}
 
 
     def accept_cookies(self): 
@@ -58,7 +66,11 @@ class MetaCriticScraper:
         for url in page_container:
             link_to_page = url.get_attribute('href')
             page_links_list.append(link_to_page)
+            self.page_counter += 1
+       
         print(page_links_list)
+        print(len(page_links_list))
+        print(f'Pages visited: {self.page_counter}')
         return page_links_list
     
     
@@ -66,21 +78,21 @@ class MetaCriticScraper:
     # TODO: Make the scraper click the next page and take the links from there. 
 
     def click_next_page(self):
-        next_page_element = self.driver.find_elements(By.XPATH, '//li[@class="page"]/a')
+        next_page_element = self.driver.find_elements(By.XPATH, '//li[@class="page_num"]')
 
         page_list = []
 
-        base_url = 'https://www.metacritic.com/browse/games/genre/date/action/all?page=1'
+        # TODO: This line isn't doing anything. How to integrate it with the code? 
+        # base_url = 'https://www.metacritic.com/browse/games/genre/date/action/all?page=1'
 
         for element in next_page_element:
             link_to_next_page = element.get_attribute('href')
             page_list.append(link_to_next_page)
 
-        # print(page_list)
+        print(page_list)
         
         return page_list
-
-            
+         
 
     #TODO: make a method which makes the scraper go to these links
     # This link should help: 
@@ -90,33 +102,42 @@ class MetaCriticScraper:
         for url in self.collect_page_links():
             self.driver.get(url)
 
+        
+      
     
-    
-    #TODO: work on this method after completing the links 
+    #TODO: work on this method after completing the link functionality. 
 
-    # def get_information_from_page(self):
+    def get_information_from_page(self):
 
+            self.xpaths_dict = {'Title': '//*[@class="hover_none"]', 
+                   'Platform': '//*[@id="main"]/div/div[1]/div[1]/div[1]/div[2]/span', 
+                   'Release_Date': '//*[@id="main"]/div/div[1]/div[1]/div[1]/div[3]/ul/li[2]/span[2]',
+                   'MetaCritic_Score': '//*[@id="main"]/div/div[1]/div[1]/div[3]/div/div/div[2]/div[1]/div[1]/div/div/a/div',
+                   'User_Score': '//*[@id="main"]/div/div[1]/div[1]/div[3]/div/div[2]/div[1]/div[2]/div[1]/div/a/div',
+                   'Description': '//li[@class="summary_detail product_summary"]' } 
             
-    #         for key,xpath in self.xpaths_dict:
-    #             try: 
-    #                 web_element = self.driver.find_element(By.XPATH, xpath).text
-    #                 self.information_dict.update({key : web_element})
+            #TODO: This could be a staticmethod? 
+            for key,xpath in self.xpaths_dict.items():
+                try: 
+                    web_element = self.driver.find_element(By.XPATH, xpath)
+                    self.information_dict[key] = web_element.text
 
-    #             except:
-    #                 self.information_dict.update({[key]: 'Null'})
-    #             print(self.information_dict)
-    #     return self.information_dict
+                except:
+                    self.information_dict[key] = 'Null'
+
+            print(self.information_dict)
+            return self.information_dict
  
-    
 
     # new syntax for driver.find_elements(By.XPATH, "xpath string")
       
 new_scraper = MetaCriticScraper()
-new_scraper.choose_game_category()
-new_scraper.choose_genre()
-new_scraper.collect_page_links()
+# new_scraper.choose_game_category()
+# new_scraper.choose_genre()
+# new_scraper.collect_page_links()
 # new_scraper.process_page_links()
-new_scraper.click_next_page()
+new_scraper.get_information_from_page()
+
 
 
     
